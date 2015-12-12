4618b035445f8512e51976489795619da19c3c89
Belgaux <oscarbelgau@gmail.com>
2015-12-12 18:23:19 +0100

    fixed imports

---
+++ day03/solution_part2.py
@@ -0,0 +1,14 @@
+from day03.solution import Santa
+
+class RoboSanta(Santa):
+    def __init__(self, x=0, y=0):
+        super().__init__(x, y)
+
+    def move():
+        pass
+
+def main():
+    s = Santa()
+    s.move('^')
+    r = RoboSanta()
+    print("asd")
