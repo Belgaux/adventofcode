1daa43994b46f0a35c97938cb4408c59c0ee7ef1
Belgaux <oscarbelgau@gmail.com>
2015-12-12 17:51:28 +0100

    day 2 part 2

---
+++ day02/solution_part2.py
@@ -0,0 +1,17 @@
+def ribbon_length(l, w, h):
+    # sort the dimensions
+    dims = sorted([l, w, h])
+    # get the two smallest numbers
+    small = dims[:-1]
+    # calculate ribbon
+    ribbon = l * w * h
+    return 2 * small[0] + 2 * small[1] + ribbon
+
+with open("input.txt") as f:
+    total = 0
+    for present in f.readlines():
+        # get dimensions
+        args = map(int, present.split('x'))
+        # unpack list into function
+        total += ribbon_length(*args)
+    print("Total feet of ribbon: ", total)
