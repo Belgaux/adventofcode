574e5a0b5e0df8aea3d79554c7be65e10dfcb061
Belgaux <oscarbelgau@gmail.com>
2015-12-12 16:52:39 +0100

    day01 solution

---
+++ day01/solution.py
@@ -0,0 +1,8 @@
+with open("input.txt") as f:
+    floor = 0;
+    for char in f.read():
+        if char == '(':
+            floor += 1
+        elif char == ')':
+            floor -= 1
+    print(floor)
