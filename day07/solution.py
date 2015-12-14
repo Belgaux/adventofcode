145303107a4b606ebaa8b16713c4b2aba1ec1190
Belgaux <oscarbelgau@gmail.com>
2015-12-14 12:22:54 +0100

    asd

--- day07/solution.py
+++ day07/solution.py
@@ -1,8 +1,40 @@
+inputs = []
 with open("input.txt") as f:
-    lines = f.read().splitlines()
-    for line in lines:
-        print(line.split(" "))
+    inputs = f.read().splitlines()
+
+class Wire():
+    def __init__(self, input, output):
+        self.input = input
+        self.output = output
+
+class ControlUnit():
+    def __init__(self):
 
 class ALU():
-    def __init__(self, operator):
+
+    operators = ("AND", "OR", "NOT", "LSHIFT", "RSHIFT")
+
+    def __init__(self, op1, op2, operator):
+        self.op1 = op1
+        self.op2 = op2
         self.operator = operator
+
+    def decode_instruction(self, args):
+        print(args)
+
+    def left_shift(self, op1, op2):
+        return op1 << op2
+
+class Circuit():
+    def __init__(self):
+        self.wires = {}
+
+
+def main():
+    a = ALU()
+    print(inputs[1])
+    a.decode_instruction(["123", "->", "x"])
+    print(a.left_shift())
+
+if __name__ == '__main__':
+    main()
