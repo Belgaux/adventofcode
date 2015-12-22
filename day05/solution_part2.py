06ad9d297c434d5d3ffbf33db5898967d713cb7b
Oscar <oscarbelgau@gmail.com>
2015-12-22 04:32:27 +0100

    added files and completed day5 part 2

---
+++ day05/solution_part2.py
@@ -0,0 +1,23 @@
+import re
+
+# matches a pattern like xyx, efe, aaa, bcb
+pattern1 = re.compile(r"(.).\1")
+# matches two recurring pairs
+pattern2 = re.compile(r"(..).*\1")
+
+def is_nice(string):
+    if pattern1.search(string) is None or pattern2.search(string) is None:
+        return False
+    # true if both patterns match
+    return True
+
+def main():
+    nice_count = 0
+    with open("input.txt") as f:
+        for string in f.readlines():
+            if is_nice(string):
+                nice_count += 1
+    print(nice_count)
+
+if __name__ == '__main__':
+    main()
