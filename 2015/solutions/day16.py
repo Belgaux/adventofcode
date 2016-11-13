2b9568bbebefc0b50b7922f0fc9e74f9461be19e
Belgaux <oscarbelgau@gmail.com>
2016-11-13 20:22:33 +0100

    day 16 part 1 solution

--- 2015/solutions/day16.py
+++ 2015/solutions/day16.py
@@ -1,6 +1,17 @@
 def main():
-    # solved part 1 with ctrl-f in the input file
-    # todo: solve it properly, with regex  
+    text = [line.replace(":", "").replace(",", "").strip().split() for line in open("../inputs/input16.txt").readlines()]
+    
+    sue = {}
+    for t in text:
+        sue[int(t[1])] = [(t[2], int(t[3])), (t[4], int(t[5])), (t[6], int(t[7]))]
+    
+    ticker_tape = set([("children", 3), ("cats", 7), ("samoyeds", 2), \
+                ("pomeranians", 3), ("akitas", 0), ("vizslas", 0), \
+                ("goldfish", 5), ("trees", 3), ("cars", 2), ("perfumes", 1)])
+    for key, value in sue.items():
+        if set(value).issubset(ticker_tape):
+            print(key)
+    
     
 if __name__ == "__main__":
     main()
