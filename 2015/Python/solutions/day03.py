8c193b3fb0f22483986d72c2750ea81af972a010
Belgaux <oscarbelgau@gmail.com>
2016-11-23 02:35:12 +0100

    merged day3 files

--- 2015/Python/solutions/day03.py
+++ 2015/Python/solutions/day03.py
@@ -1,26 +1,50 @@
 class Santa():
     def __init__(self):
-        self.x = 0
-        self.y = 0
+        self.x_pos = 0
+        self.y_pos = 0
+        self.pos = (self.x_pos, self.y_pos)
+
+    def set_pos(self):
+        self.pos = (self.x_pos, self.y_pos)
 
     def move(self, direction):
         if direction == '^':
-            self.y += 1
+            self.y_pos += 1
         elif direction == 'v':
-            self.y -= 1
+            self.y_pos -= 1
         elif direction == '<':
-            self.x -= 1
+            self.x_pos -= 1
         elif direction == '>':
-            self.x += 1
-        self.pos = (self.x, self.y)
+            self.x_pos += 1
+        self.set_pos()
 
 def main():
+    # part 1
     houses = {}
     santa = Santa()
-    with open("../inputs/input3.txt") as f:
-        for d in f.read().strip('\n'):
-            santa.move(d)
+    with open("../inputs/input3.txt") as in_file:
+        for dest in in_file.read().strip('\n'):
+            santa.move(dest)
             houses[santa.pos] = True
+    print("houses with at least 1 present: ", len(houses))
+
+    # part 2
+    houses = {}
+    santa = Santa()
+    robot = Santa()
+
+    stack = []
+    with open("../inputs/input3.txt") as in_file:
+        for dest in in_file.read().strip('\n'):
+            stack.append(dest)
+    stack.reverse()
+    while stack:
+        if stack:
+            santa.move(stack.pop())
+            houses[santa.pos] = True
+        if stack:
+            robot.move(stack.pop())
+            houses[robot.pos] = True
     print("houses with at least 1 present: ", len(houses))
 
 if __name__ == "__main__":
