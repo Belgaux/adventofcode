d6c959d2342ad97a95cb6f7f1c736ab0045ceb9b
Belgaux <oscarbelgau@gmail.com>
2016-11-23 02:39:27 +0100

    merged day 5

--- 2015/Python/solutions/day05.py
+++ 2015/Python/solutions/day05.py
@@ -5,6 +5,18 @@ bad_sub_strings = ["ab", "cd", "pq", "xy"]
 # finds substrings of two recurring chars
 pattern = re.compile(r"(.)\1{1,}")
 
+# matches a pattern like xyx, efe, aaa, bcb
+pattern1 = re.compile(r"(.).\1")
+# matches two recurring pairs
+pattern2 = re.compile(r"(..).*\1")
+
+def is_nice_day2(string):
+    if pattern1.search(string) is None \
+            or pattern2.search(string) is None:
+        return False
+    else:
+        return True
+
 def vowel_count(string):
     vowels = "aeiou"
     count = 0
@@ -23,11 +35,16 @@ def is_nice(string):
 
 def main():
     nice_count = 0
+    nice_count_day2 = 0
     with open("../inputs/input5.txt") as f:
         for string in f.readlines():
             if is_nice(string):
                 nice_count += 1
+            if is_nice_day2(string):
+                nice_count_day2 += 1
     print(nice_count)
+    print(nice_count_day2)
+
 
 if __name__ == '__main__':
     main()
