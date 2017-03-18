fee98c93774b5d24d8c2f06b32315a365dc3af7e
Belgaux <oscarbelgau@gmail.com>
2017-03-18 22:25:42 +0100

    day 3 part 2

--- 2016/Python/solutions/day03.py
+++ 2016/Python/solutions/day03.py
@@ -2,10 +2,21 @@ def is_triangle(a, b, c):
     return (a + b) > c and (b + c) > a and (a + c) > b
 
 count = 0
-for line in open("../inputs/03.txt").readlines():
-    args = list(map(int, line.split()))
-    if is_triangle(*args):
-        print(args)
-        count += 1
-     
+with open("../inputs/03.txt") as fp:
+    for line in fp:
+        args = list(map(int, line.split()))
+        if is_triangle(*args):
+            count += 1
+print(count)
+
+# part 2
+lines = [list(map(int, line.strip().split())) 
+            for line in open("../inputs/03.txt")]
+count = 0
+for col in range(3):
+    for i in range(0, len(lines)-2, 3):
+        if is_triangle(lines[i][col], 
+                lines[i+1][col], 
+                lines[i+2][col]):
+            count += 1
 print(count)
