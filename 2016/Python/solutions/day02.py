792021d88464aa246b01b2d7666533d2a88651b9
Belgaux <oscarbelgau@gmail.com>
2017-03-18 13:08:57 +0100

    day 2 part 2

--- 2016/Python/solutions/day02.py
+++ 2016/Python/solutions/day02.py
@@ -1,4 +1,3 @@
-
 keypad = [[1,2,3],[4,5,6],[7,8,9]]
 pos = [1,1]
 
@@ -17,3 +16,35 @@ for line in open("../inputs/02.txt").readlines():
             if pos[1] - 1 >= 0:
                 pos[1] -= 1
     print(keypad[pos[0]][pos[1]], end="")
+
+# part 2
+print()
+
+keypad = [[0,  0,   1,   0,  0],
+          [0,  2,   3,   4,  0],
+          [5,  6,   7,   8,  9],
+          [0, "A", "B", "C", 0],
+          [0,  0,  "D",  0,  0]]
+pos = [2, 0]
+
+for line in open("../inputs/02.txt").readlines():
+    for char in line:
+        r, c = pos[0], pos[1]
+        try:
+            if char is "U":
+                if keypad[r-1][c] != 0:
+                    pos[0] -= 1
+            elif char is "D":
+                if keypad[r+1][c] != 0:
+                    pos[0] += 1
+            elif char is "R":
+                if keypad[r][c+1] != 0:
+                    pos[1] += 1
+            elif char is "L":
+                if keypad[r][c-1] != 0:
+                    pos[1] -= 1
+        except:
+            # index error, edge
+            continue
+    print(keypad[pos[0]][pos[1]], end="")
+
