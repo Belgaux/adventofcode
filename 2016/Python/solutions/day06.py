4c119b9c6873d947dc3a8e420b266c5e386c8f61
belgaux <oscarbelgau@gmail.com>
2017-06-18 21:13:18 +0200

    day 6 part 1

---
+++ 2016/Python/solutions/day06.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+msg = [ line.rstrip() for line in open("../inputs/06.txt").readlines() ]
+cols = [ [row[i] for row in msg] for i in range(len(msg[0])) ]
+
+# find char frequency for each column
+for col in cols:
+    d = defaultdict(int)
+    for c in col:
+        d[c] += 1
+    print(max(d.items(), key=lambda x: x[1]))
