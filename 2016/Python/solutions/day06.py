5633393b896b0f993de3b5a67d94b4eec911e9fd
belgaux <oscarbelgau@gmail.com>
2017-06-19 21:17:38 +0200

    Exploring the std lib, using collections.Counter

--- 2016/Python/solutions/day06.py
+++ 2016/Python/solutions/day06.py
@@ -1,13 +1,11 @@
-from collections import defaultdict
-msg = [ line.rstrip() for line in open("../inputs/06.txt").readlines() ]
-cols = [ [row[i] for row in msg] for i in range(len(msg[0])) ]
+msg = [line.rstrip() for line in open("../inputs/06.txt")]
+cols = [[row[i] for row in msg] for i in range(len(msg[0]))]
 
-# find char frequency for each column
-for col in cols:
-    d = defaultdict(int)
-    for c in col:
-        d[c] += 1
-    # part 1
-    #print(max(d.items(), key=lambda x: x[1]))
-    # part 2
-    print(min(d.items(), key=lambda x: x[1]))
+from collections import Counter
+most_common = [Counter(col).most_common()[0] for col in cols]
+least_common = [Counter(col).most_common()[-1] for col in cols]
+
+corrected_msg = "".join(x[0] for x in most_common)
+corrected_msg_2 = "".join(x[0] for x in least_common)
+print(corrected_msg)
+print(corrected_msg_2)
