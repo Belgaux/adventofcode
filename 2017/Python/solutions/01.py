6177300a63993ab9b18a77a830b06d884b79d171
Belgaux <oscarbelgau@gmail.com>
2017-12-03 01:41:44 +0100

    Day 1 part 2

--- 2017/Python/solutions/01.py
+++ 2017/Python/solutions/01.py
@@ -1,7 +1,12 @@
 
-def captcha_sum(sequence):
-  return sum([int(num) for i, num in enumerate(sequence) 
-      if sequence[i+1 if i < len(sequence)-1 else 0] == num])
+def captcha_sum(seq):
+  return sum([int(num) for i, num in enumerate(seq) 
+      if seq[i+1 if i < len(seq)-1 else 0] == num])
+
+def captcha_sum_halfway(seq):
+  return sum([int(num) for i, num in enumerate(seq) 
+      if seq[int((i + len(seq)/2) % len(seq))] == num])
+
 
 
 assert(captcha_sum("1122") == 3)
@@ -9,6 +14,13 @@ assert(captcha_sum("1111") == 4)
 assert(captcha_sum("1234") == 0)
 assert(captcha_sum("91212129") == 9)
 
+assert(captcha_sum_halfway("1212") == 6)
+assert(captcha_sum_halfway("1221") == 0)
+assert(captcha_sum_halfway("123425") == 4)
+assert(captcha_sum_halfway("123123") == 12)
+assert(captcha_sum_halfway("12131415") == 4)
 
 with open("../inputs/01.txt") as f:
-  print(captcha_sum(f.read().strip()))
+  puzzle_seq = f.read().strip()
+  print(captcha_sum(puzzle_seq))
+  print(captcha_sum_halfwa(puzzle_seq))
