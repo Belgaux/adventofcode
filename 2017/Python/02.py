b3f6db483daeb5cd058fb40c2b4b1bed02bbda5e
Belgaux <oscarbelgau@gmail.com>
2017-12-05 23:15:45 +0100

    Day 2 part 2

--- 2017/Python/02.py
+++ 2017/Python/02.py
@@ -2,11 +2,22 @@
 def checksum(sheet):
   return sum([max(line) - min(line) for line in sheet])
 
+def checksum_div(sheet):
+  s = 0
+  for line in sheet:
+    for num in line:
+      for other in line:
+        if num != other and num % other == 0:
+          s += num / other
+  return s
+
 
 assert checksum([[5,1,9,5], [7,5,3], [2,4,6,8]]) == 18
+assert checksum_div([[5,9,2,8], [9,4,7,3], [3,8,6,5]]) == 9
 
 with open("../inputs/02.txt") as f:
   sheet = [line.strip().split("\t") for line in f.readlines()]
   sheet = [[int(e) for e in line] for line in sheet]
   print(checksum(sheet))
+  print(checksum_div(sheet))
 
