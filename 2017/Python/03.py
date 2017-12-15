83d0ea3fabf1178c0c3fa8f1299fb4e25c86cb3e
belgaux <oscarbelgau@gmail.com>
2017-12-15 03:14:10 +0100

    Day 2 part 2

--- 2017/Python/03.py
+++ 2017/Python/03.py
@@ -1,3 +1,6 @@
+import itertools
+import sys
+import collections
 
 def step_length(move, k):
   """
@@ -6,6 +9,7 @@ def step_length(move, k):
   """ 
   moves = {"R": 2*k+1, "U": 2*k+1, "L": 2*k+2, "D": 2*k+2} 
   return moves[move]
+
 
 def next_coords(coords, move):
   x, y = coords
@@ -18,6 +22,16 @@ def next_coords(coords, move):
   elif move == "D":
     return (x, y-1)
 
+
+def sum_neighbors(coords, grid):
+  neighborhood = itertools.product(range(-1, 2), range(-1, 2))
+  s = 0
+  for n in neighborhood:
+    neighbor_coord = tuple(a+b for a, b in zip(coords, n))
+    s += grid[neighbor_coord]
+  return s
+
+
 def number_spiral_coords(n):
   """Generate a number spiral to find coordinates of number n"""
   move_order = ["R", "U", "L", "D"]
@@ -25,10 +39,22 @@ def number_spiral_coords(n):
   current_block = 0
   current_coords = (0, 0)
 
+  grid = collections.defaultdict(int)
+  grid[(0,0)] = 1
+  found = False
+
   done = False
   while (not done):
     for move in move_order:
       for _ in range(step_length(move, current_block)):
+
+        if not found:
+          ns = sum_neighbors(current_coords, grid)
+          grid[current_coords] = ns
+          if ns > n and not found:
+            print("First value written larger than input: ", ns)
+            found = True
+
         current_coords = next_coords(current_coords, move)
         current_square += 1
         if current_square == n:
