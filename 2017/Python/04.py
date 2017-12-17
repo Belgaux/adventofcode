add528136d6a1c2d4357cf2f5cf34281e9ded1b5
belgaux <oscarbelgau@gmail.com>
2017-12-17 06:02:49 +0100

    Day 4 part 2

--- 2017/Python/04.py
+++ 2017/Python/04.py
@@ -1,12 +1,22 @@
 import collections
+import itertools
 
-def valid_passphrase(phrase):
+def valid_passphrase(phrase, part_two=False):
   unique = collections.defaultdict(int)
   for word in phrase.split():
-    unique[word] += 1
+    if part_two:
+      unique_permutations = set(itertools.permutations(word))
+      for perm in unique_permutations:
+        unique[perm] += 1
+    else:
+      unique[word] += 1
   return all(v is 1 for k, v in unique.items())
 
 
 with open("../inputs/04.txt") as f:
-  print(list((valid_passphrase(line) for line in f)).count(True))
+  puzzle_input = list(line.strip() for line in f)
+  print(list((valid_passphrase(line) 
+        for line in puzzle_input)).count(True))
+  print(list((valid_passphrase(line, part_two=True)
+        for line in puzzle_input)).count(True))
 
